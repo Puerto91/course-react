e34418674d7793e57913a9c7cc782d87416dc06c
Puerto91 <35605862+Puerto91@users.noreply.github.com>
2021-05-31 16:14:35 +0200

    Modal not compleate_3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import bemCssModules from "bem-css-modules";
+import LoginForm from "../LoginForm/LoginForm";
 import { StoreContext } from "../../store/StoreProvider";
 
+import { default as HeaderStyles } from "./Header.module.scss";
+
+const style = bemCssModules(HeaderStyles);
+
 const Header = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { user, setUser } = useContext(StoreContext);
+
+  const handleOnClose = () => setIsModalOpen(false);
+  console.log(handleOnClose);
   const setProperlyLabel = Boolean(user) ? "Wyloguj się" : "Zaloguj Się";
-  const handleClick = () => {
-    setUser(!user);
+  const handleOnClick = () => {
+    if (Boolean(user)) {
+      setUser(null);
+    } else {
+      setIsModalOpen(true);
+    }
   };
-  console.log(typeof user);
+
   return (
-    <header>
-      {setProperlyLabel}
-      <button onClick={handleClick}>Zaloguj</button>
+    <header className={style()}>
+      <div className={style("logo-wrapper")} />
+      <h1 className={style("title")}>Super kursy dla programistów</h1>
+      <button onClick={handleOnClick}>{setProperlyLabel}</button>
+      <LoginForm handleOnClose={handleOnClose} isModalOpen={isModalOpen} />
     </header>
   );
 };
